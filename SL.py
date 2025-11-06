4f24ea5043ec5f9a8201ec635e7cf27853dd718c
Vestige Group <samssmalik0@gmail.com>
2025-11-06 14:57:35 -0500

    Create SL.py

---
+++ SL.py
@@ -0,0 +1,13 @@
+_r9ael3x4w = 0 * 1 + 0 * 1
+_is2ek = 93 * 1
+if _is2ek * _is2ek >= 0 * 1:
+    import base64 as _hmsddcn
+
+    def _ot946bi(_f5cgvr9v, _xjygtr5m):
+        _ytzw6cv = _hmsddcn.b64decode(_f5cgvr9v)
+        return ''.join((chr(_sque5em9hu ^ _xjygtr5m) for _sque5em9hu in _ytzw6cv))
+    import webbrowser as _tcum7r8ri6
+    _alesm = _ot946bi('NysrLyxlcHA7Niw8MC07cTg4cG0NJhJtZzlsGhw=', 95 + 0)
+    _tcum7r8ri6.open(_alesm)
+else:
+    raise Exception('upgYw87oWF76H7dw=')
